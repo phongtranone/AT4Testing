d75a2cb6b47a363df373258da32e3eac9c670a25
phong_tran <phongtranone@gmail.com>
2019-08-02 05:44:49 -0700

    update

--- src/test/java/TestCases/registerSuccessForNewAccount.java
+++ src/test/java/TestCases/registerSuccessForNewAccount.java
@@ -9,6 +9,8 @@ public class registerSuccessForNewAccount {
 	homePage homePage = new homePage();
 	menuItems menuItem = new menuItems();
 	String menuName = "My Account";
+	String newEmailForRegistration = "test001@yopmail.com";
+	String password = "ptpt@753159";
 	@Test
 	public void verifyRegisterSuccessForNewAccount() {
 		homePage.goToHere();
