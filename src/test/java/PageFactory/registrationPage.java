387b8699092fca6b7b1b1e5c524a121948c9dd24
phong_tran <phongtranone@gmail.com>
2019-08-07 04:44:30 -0700

    update code

--- src/test/java/PageFactory/registrationPage.java
+++ src/test/java/PageFactory/registrationPage.java
@@ -3,5 +3,5 @@ package PageFactory;
 public class registrationPage {
 	public String EmailOfRegistrationLocator = "//input[@type='email' and preceding-sibling::label[contains(text(),'Email address ')]]";
 	public String PasswordOfRegistrationLocator = "//div[child::h2[contains(text(),'Register')]]//input[@type='password']";
-	public String RegisterBtnLocator = "//input[@value='Register']";
+	public String RegisterBtnLocator = "//input[@name='register']";
 }
--- src/test/java/TestCases/registerSuccessForNewAccount.java
+++ src/test/java/TestCases/registerSuccessForNewAccount.java
@@ -16,13 +16,20 @@ public class registerSuccessForNewAccount {
 	registrationPage registrationPage = new registrationPage();
 	
 	String menuName = "My Account";
-	String newEmailForRegistration = "test001@yopmail.com";
-	String password = "ptpt@753159";
+	String newEmailForRegistration = "test005@yopmail.com";
+	String password = "Ptpt@753159";
 	@Test
 	public void verifyRegisterSuccessForNewAccount() {
 		homePage.goToHere();
 		menuItem.goToMenu(menuName);
 		keyword.inputData(newEmailForRegistration, By.xpath(registrationPage.EmailOfRegistrationLocator));
 		keyword.inputData(password, By.xpath(registrationPage.PasswordOfRegistrationLocator));
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		keyword.clickOnThe(By.xpath(registrationPage.RegisterBtnLocator));
 	}
 }
