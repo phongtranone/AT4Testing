70312ec107b5c980cedc2580b6178350acafd8d9
phong_tran <phongtranone@gmail.com>
2019-08-13 04:04:06 -0700

    It is ok

--- src/test/java/Keywords/SimpleKeyword.java
+++ src/test/java/Keywords/SimpleKeyword.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import TestInit.ConstantLib;
 
@@ -29,5 +32,11 @@ public class SimpleKeyword {
 	public void inputData(String data, By by) {
 		WebElement element = ConstantLib.driver.findElement(by);
 		element.sendKeys(data);
+	}
+	
+	public void waitForElementPresent(By by) {
+		WebDriverWait wait = new WebDriverWait(ConstantLib.driver, 40);
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
+		
 	}
 }
--- src/test/java/TestCases/registerSuccessForNewAccount.java
+++ src/test/java/TestCases/registerSuccessForNewAccount.java
@@ -1,6 +1,7 @@
 package TestCases;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 
 import HighLevelFunctions.menuItems;
@@ -18,18 +19,19 @@ public class registerSuccessForNewAccount {
 	String menuName = "My Account";
 	String newEmailForRegistration = "test005@yopmail.com";
 	String password = "Ptpt@753159";
+	String validation_locator = "//div[@aria-live='polite']";
+	
 	@Test
 	public void verifyRegisterSuccessForNewAccount() {
 		homePage.goToHere();
 		menuItem.goToMenu(menuName);
 		keyword.inputData(newEmailForRegistration, By.xpath(registrationPage.EmailOfRegistrationLocator));
 		keyword.inputData(password, By.xpath(registrationPage.PasswordOfRegistrationLocator));
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		ConstantLib.driver.findElement(By.xpath(registrationPage.PasswordOfRegistrationLocator)).sendKeys(Keys.NUMPAD0);
+		ConstantLib.driver.findElement(By.xpath(registrationPage.PasswordOfRegistrationLocator)).sendKeys(Keys.NUMPAD0);
+		ConstantLib.driver.findElement(By.xpath(registrationPage.PasswordOfRegistrationLocator)).sendKeys(Keys.NUMPAD0);
+		ConstantLib.driver.findElement(By.xpath(registrationPage.PasswordOfRegistrationLocator)).sendKeys(Keys.NUMPAD0);
+		//keyword.waitForElementPresent(By.xpath(validation_locator));
 		keyword.clickOnThe(By.xpath(registrationPage.RegisterBtnLocator));
 	}
 }
